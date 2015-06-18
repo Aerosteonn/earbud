f26ddc3bcd317f217a6b9eb53e14e91ad081c15d
funkeyflo <florentijncornet@gmail.com>
2015-06-18 13:03:03 +0200

    Voice recognition in index.ejs

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var Bookshelf = require('bookshelf');
 
 var config = {
-   host: '192.168.188.146',  // your host
+   host: 'localhost',  // your host
    user: 'root', // your database user
    password: '1234', // your database password
    database: 'earbud',
